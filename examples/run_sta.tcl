caf8e51807f6ff970015dd8f97d14a23f97b140c
VSD <vsd@vlsisystemdesign.com>
2026-09-19 13:51:41 +0530

    Add minimal OpenSTA smoke test script

---
+++ examples/run_sta.tcl
@@ -0,0 +1,9 @@
+# Minimal OpenSTA smoke test.
+# Run from the repository root:
+# sta -no_splash -no_init examples/run_sta.tcl
+
+puts "OpenSTA is running inside the VSD Codespace."
+puts "STA executable: [info nameofexecutable]"
+puts "Tcl version: [info patchlevel]"
+
+exit
